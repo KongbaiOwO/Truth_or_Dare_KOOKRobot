02354e74c85288ae6d7052ae9c706ceb4281d2f2
空白白 <99713809+KongbaiOwO@users.noreply.github.com>
2022-07-23 10:30:47 +0800

    主程序

--- main.py
+++ main.py
@@ -42,22 +42,27 @@ def reset(gid):
     del mkdir
 
 
-bot = Bot(token='请输入你自己的机器人秘钥')
+bot = Bot(token='1/MTIwMTQ=/GoaEMe4ppT62xkcJHz5NYg==')
 
 @bot.command(name = '重置真心话大冒险' )
 async def restcommand(msg: Message):
     print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
-    reset(msg.extra['guild_id'])
-    await msg.ctx.channel.send("重置成功")
-    c = Card(Module.Header('真心话大冒险'),
-             Module.Context('加入或退出真心话大冒险'),
-             Module.ActionGroup(
-                 Element.Button('加入', '加入真心话大冒险', Types.Click.RETURN_VAL, Types.Theme.INFO),
-                 Element.Button('退出', '退出真心话大冒险', Types.Click.RETURN_VAL, Types.Theme.DANGER),
-                 Element.Button('开始真心话大冒险', '开始真心话大冒险', Types.Click.RETURN_VAL, Types.Theme.PRIMARY))
-             )
-    cm = CardMessage(c)
-    await msg.ctx.channel.send(cm)
+    with open('./管理员.txt' , 'r' , encoding='utf-8') as f:
+        admin=f.read().split('+')
+    if msg.author_id not in admin:
+        await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 你没有权限使用此命令''', temp_target_id=msg.author_id)
+    else:
+        reset(msg.extra['guild_id'])
+        await msg.ctx.channel.send("重置成功")
+        c = Card(Module.Header('真心话大冒险'),
+                 Module.Context('加入或退出真心话大冒险'),
+                 Module.ActionGroup(
+                     Element.Button('加入', '加入真心话大冒险', Types.Click.RETURN_VAL, Types.Theme.INFO),
+                     Element.Button('退出', '退出真心话大冒险', Types.Click.RETURN_VAL, Types.Theme.DANGER),
+                     Element.Button('开始真心话大冒险', '开始真心话大冒险', Types.Click.RETURN_VAL, Types.Theme.PRIMARY))
+                 )
+        cm = CardMessage(c)
+        await msg.ctx.channel.send(cm)
 
 @bot.command(name = '真心话大冒险' )
 async def menucommand(msg: Message):
@@ -155,7 +160,7 @@ async def print_btn_value(b: Bot, e: Event):
         if data['选择的惩罚']!='' or int(e.body['user_id'])!=data['惩罚玩家']:
             await b.send(channel, f'''(met){e.body['user_id']}(met) 你现在无需选择''', temp_target_id=e.body['user_id'])
         else:
-            with open('真心话词库.txt', encoding='utf-8') as f:
+            with open('真心话词库.txt', 'r', encoding='utf-8') as f:
                 zxh=f.read().split("+")
             from random import choice
             punish=choice(zxh)
@@ -174,7 +179,7 @@ async def print_btn_value(b: Bot, e: Event):
         if data['选择的惩罚']!='' or int(e.body['user_id'])!=data['惩罚玩家']:
             await b.send(channel, f'''(met){e.body['user_id']}(met) 你现在无需选择''', temp_target_id=e.body['user_id'])
         else:
-            with open('大冒险词库.txt', encoding='utf-8') as f:
+            with open('大冒险词库.txt', 'r' ,encoding='utf-8') as f:
                 zxh=f.read().split("+")
             from random import choice
             punish=choice(zxh)
@@ -422,6 +427,162 @@ async def checkpoints(msg: Message):
                      )
             cm = CardMessage(c)
             await msg.ctx.channel.send(cm)
+
+@bot.command(name = '查真心话')
+async def inquirezxh(msg: Message):
+    print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
+    with open('真心话词库.txt', 'r', encoding='utf-8') as f:
+        zxh=f.read().split('+')
+    reply=''
+    page=1
+    for i in range(len(zxh)):
+        reply+=f'{i+1}. {zxh[i]} \n'
+        if (i+1)%20 == 0 or i+1==len(zxh):
+            c = Card(Module.Header(f'共有{len(zxh)}个真心话（第{page}/{(len(zxh)+19)//20}页）：'),
+                     Module.Context(reply))
+            cm=CardMessage(c)
+            reply=''
+            page+=1
+            await msg.ctx.channel.send(cm)
+
+@bot.command(name = '查大冒险')
+async def inquiredmx(msg: Message):
+    print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
+    with open('大冒险词库.txt', 'r', encoding='utf-8') as f:
+        dmx=f.read().split('+')
+    reply=''
+    page=1
+    for i in range(len(dmx)):
+        reply+=f'{i+1}. {dmx[i]} \n'
+        if (i+1)%20 == 0 or i+1==len(dmx):
+            c = Card(Module.Header(f'共有{len(dmx)}个大冒险（第{page}/{(len(dmx)+19)//20}页）：'),
+                     Module.Context(reply))
+            cm=CardMessage(c)
+            reply=''
+            page+=1
+            await msg.ctx.channel.send(cm)
+
+@bot.command(name = '删真心话')
+async def delzxh(msg: Message , number:int):
+    print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
+    with open('./管理员.txt', 'r', encoding='utf-8') as f:
+        admin = f.read().split('+')
+    if msg.author_id not in admin:
+        await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 你没有权限使用此命令''', temp_target_id=msg.author_id)
+    else:
+        with open('真心话词库.txt', 'r', encoding='utf-8') as f:
+            zxh=f.read().split('+')
+        if number > len(zxh):
+            await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 无此真心话，共{len(zxh)}个真心话''')
+        else:
+            reply=zxh[number-1]
+            del zxh[number-1]
+            writezxh='+'.join(zxh)
+            with open('真心话词库.txt', 'w', encoding='utf-8') as f:
+                f.write(writezxh)
+            await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 删除真心话\"{reply}\"成功，现在共{len(zxh)}个真心话''')
+
+@bot.command(name = '删大冒险')
+async def deldmx(msg: Message , number:int):
+    print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
+    with open('./管理员.txt', 'r', encoding='utf-8') as f:
+        admin = f.read().split('+')
+    if msg.author_id not in admin:
+        await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 你没有权限使用此命令''', temp_target_id=msg.author_id)
+    else:
+        with open('大冒险词库.txt', 'r', encoding='utf-8') as f:
+            dmx=f.read().split('+')
+        if number>len(dmx):
+            await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 无此大冒险，共{len(dmx)}个大冒险''')
+        else:
+            reply=dmx[number-1]
+            del dmx[number-1]
+            writedmx='+'.join(dmx)
+            with open('大冒险词库.txt', 'w', encoding='utf-8') as f:
+                f.write(writedmx)
+            await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 删除大冒险\"{reply}\"成功，现在共{len(dmx)}个大冒险''')
+
+@bot.command(name = '加真心话')
+async def addzxh(msg: Message , add:str):
+    print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
+    with open('./管理员.txt', 'r', encoding='utf-8') as f:
+        admin = f.read().split('+')
+    if msg.author_id not in admin:
+        await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 你没有权限使用此命令''', temp_target_id=msg.author_id)
+    else:
+        with open('真心话词库.txt', 'r', encoding='utf-8') as f:
+            zxh=f.read().split('+')
+        zxh.append(add)
+        writezxh='+'.join(zxh)
+        with open('真心话词库.txt', 'w', encoding='utf-8') as f:
+            f.write(writezxh)
+        await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 添加真心话\"{add}\"成功，现在共{len(zxh)}个真心话''')
+
+@bot.command(name = '加大冒险')
+async def adddmx(msg: Message , add:str):
+    print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
+    with open('./管理员.txt', 'r', encoding='utf-8') as f:
+        admin = f.read().split('+')
+    if msg.author_id not in admin:
+        await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 你没有权限使用此命令''', temp_target_id=msg.author_id)
+    else:
+        with open('大冒险词库.txt', 'r', encoding='utf-8') as f:
+            dmx=f.read().split('+')
+        dmx.append(add)
+        writedmx='+'.join(dmx)
+        with open('大冒险词库.txt', 'w', encoding='utf-8') as f:
+            f.write(writedmx)
+        await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 添加大冒险\"{add}\"成功，现在共{len(dmx)}个大冒险''')
+
+@bot.command(name = '呼叫管理员')
+async def calladmin(msg: Message):
+    print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
+    with open('./管理员.txt', 'r', encoding='utf-8') as f:
+        admin = f.read().split('+')
+    reply=''
+    for i in admin:
+        reply+=f'(met){i}(met) '
+    await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 正在呼叫管理员，请管理员尽快到达战场： \n{reply}''')
+
+@bot.command(name = '加管理员')
+async def addadmin(msg: Message , add:str):
+    print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
+    with open('./管理员.txt', 'r', encoding='utf-8') as f:
+        admin = f.read().split('+')
+    if msg.author_id not in admin:
+        await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 你没有权限使用此命令''', temp_target_id=msg.author_id)
+    else:
+        add=add.replace('(met)','')
+        if add in admin:
+            await msg.ctx.channel.send(f'''(met){add}(met) 已为管理员''')
+        else:
+            admin.append(add)
+            writeadmin='+'.join(admin)
+            with open('./管理员.txt', 'w', encoding='utf-8') as f:
+                f.write(writeadmin)
+            await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 已添加 (met){add}(met) 为管理员''')
+
+@bot.command(name = '删管理员')
+async def deladmin(msg: Message , number:str):
+    print(f'''{msg.author.nickname} 触发了命令 \"{msg.extra["kmarkdown"]["raw_content"]}\"''')
+    with open('./管理员.txt', 'r', encoding='utf-8') as f:
+        admin = f.read().split('+')
+    if msg.author_id not in admin:
+        await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 你没有权限使用此命令''', temp_target_id=msg.author_id)
+    else:
+        number=number.replace('(met)','')
+        if number not in admin:
+            await msg.ctx.channel.send(f'''(met){number}(met) 不是管理员''')
+        else:
+            for i in range(len(admin)):
+                if admin[i]==number:
+                    del admin[i]
+                    break
+            writeadmin='+'.join(admin)
+            with open('./管理员.txt', 'w', encoding='utf-8') as f:
+                f.write(writeadmin)
+            await msg.ctx.channel.send(f'''(met){msg.author_id}(met) 已删除 (met){number}(met) 为管理员''')
+
 
 if __name__=="__main__":
     bot.run()
